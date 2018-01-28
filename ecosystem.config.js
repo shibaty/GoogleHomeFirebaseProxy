1c29aa46b5615836c5ae47f0363d51fc50fc1460
shibaty <shibaty@gmail.com>
2018-01-29 00:48:25 +0900

    update log format

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       name      : 'GoogleHomeFirebaseProxy',
       script    : 'lib/index.js',
-      log_date_format: "YYYY-MM-DD HH-mm-SS"
+      log_date_format: "YYYY-MM-DD HH:mm:SS"
     }
 
   ]
