537011403c6bd7112e90444e2f07394bb9b3e30b
shibaty <shibaty@gmail.com>
2018-01-31 01:43:28 +0900

    add watch

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,9 @@ module.exports = {
     {
       name      : 'GoogleHomeFirebaseProxy',
       script    : 'lib/index.js',
-      log_date_format: "YYYY-MM-DD HH:mm:SS"
+      log_date_format: "YYYY-MM-DD HH:mm:SS",
+      watch     : ["lib"],
+      
     }
 
   ]
